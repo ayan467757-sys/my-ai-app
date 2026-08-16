f1d5f64a7777f3009497c4a2a0aa59775a730470
ayan467757-sys <ayan467757@gmail.com>
2026-08-17 02:33:10 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from google import genai
+
+app = FastAPI()
+
+client = genai.Client(
+    api_key="PASTE_YOUR_API_KEY_HERE"
+)
+
+class ChatRequest(BaseModel):
+    message: str
+
+@app.get("/")
+def home():
+    return {"message": "My AI App is working!"}
+
+@app.post("/chat")
+def chat(request: ChatRequest):
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=request.message
+    )
+    return {"reply": response.text}
